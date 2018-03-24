fe93b76accc88e3efbccb51aa52bb13f4b9a88c5
delphin <delphin.aubin@gmail.com>
2018-03-24 13:15:54 +0100

    CHORE Externalise mongo configuration in application.properties

--- src/main/java/com/delphin/cocktailmanager/CocktailManagerApplication.java
+++ src/main/java/com/delphin/cocktailmanager/CocktailManagerApplication.java
@@ -2,6 +2,7 @@ package com.delphin.cocktailmanager;
 
 import com.mongodb.reactivestreams.client.MongoClients;
 import com.mongodb.reactivestreams.client.MongoClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
@@ -15,15 +16,25 @@ public class CocktailManagerApplication extends AbstractReactiveMongoConfigurati
 		SpringApplication.run(CocktailManagerApplication.class, args);
 	}
 
+	@Value("${database.name}")
+    private String databaseName;
+
+    @Value("${database.port}")
+    private String databasePort;
+
+    @Value("${database.host}")
+    private String databaseHost;
 
 	@Override
 	protected String getDatabaseName() {
-	    return "cocktail-db";
+        return databaseName;
 	}
 
 	@Override
 	public MongoClient reactiveMongoClient() {
-		return MongoClients.create("mongodb://localhost:27017");
+        return MongoClients.create(
+		        String.format("mongodb://%s:%s", databaseHost, databasePort)
+        );
 	}
 }
 
